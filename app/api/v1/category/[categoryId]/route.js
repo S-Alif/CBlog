6a6d35127efd0bb8da774b406d55fba2d8621354
S-Alif <alif160038@gmail.com>
2025-05-26 11:13:33 +0600

    DONE | ADD - category controller and service setup, added auth checking

---
+++ app/api/v1/category/[categoryId]/route.js
@@ -0,0 +1,4 @@
+import categoryController from "@/lib/controllers/category/category.controller";
+
+export const PATCH = categoryController.save
+export const DELETE = categoryController.remove
---
+++ app/api/v1/category/route.js
@@ -0,0 +1,4 @@
+import categoryController from "@/lib/controllers/category/category.controller";
+
+export const GET = categoryController.getAll
+export const POST = categoryController.save
