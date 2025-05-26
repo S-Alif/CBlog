8b8deb04a403788bb6070db156139917b86939c7
S-Alif <alif160038@gmail.com>
2025-05-26 21:46:40 +0600

    DONE - tags api, controller, services

---
+++ app/api/v1/tags/[tagId]/route.js
@@ -0,0 +1,4 @@
+import tagController from "@/lib/controllers/tags/tags.controller";
+
+export const PATCH = tagController.save
+export const DELETE = tagController.remove
---
+++ app/api/v1/tags/route.js
@@ -0,0 +1,4 @@
+import tagController from "@/lib/controllers/tags/tags.controller";
+
+export const POST = tagController.save
+export const GET = tagController.getAll
