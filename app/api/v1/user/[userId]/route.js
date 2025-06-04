b085ff8d4cd06555d48af880a99dcf23832161c2
s-alif <alif160038@gmail.com>
2025-06-04 10:28:19 +0600

    ADD - making a user moderator controller and service

--- app/api/v1/user/[userId]/route.js
+++ app/api/v1/user/[userId]/route.js
@@ -2,3 +2,4 @@ import userController from "@/lib/controllers/user/user.controller"
 
 export const PATCH = userController.updateUserStatusByAdminAndModerator
 export const GET = userController.getUserById
+export const PUT = userController.makeModerator
