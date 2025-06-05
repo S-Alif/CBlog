299b55332ed88ddb66f0c51dff1946e2c3b13264
s-alif <alif160038@gmail.com>
2025-06-05 20:03:12 +0600

    DONE  - user dashboard data controller and services

--- app/api/v1/dashboard/user/route.js
+++ app/api/v1/dashboard/user/route.js
@@ -1,5 +1,3 @@
-import userControlller from "@/lib/controllers/user/user.controller"
 import dashboardController from '@/lib/controllers/dashboard/dashboard.controller'
 
-export const PUT = userControlller.makeAdmin
 export const GET = dashboardController.userDashboard
